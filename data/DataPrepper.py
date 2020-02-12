adds class weighting and early stopping and fixes some bugs related to input pipeline

data/DataPrepper.py
@@ -24,11 +24,16 @@ class DataPrepper:
                 os.makedirs(sample_dir)
             data = loadmat(f)[column]
             n_rows = len(data)
+            labels = []
+
             for j in range(n_rows):
-                out_str = os.path.join(sample_dir, 'epoch{}.npz'.format(j+1))
-                np.savez(out_str, x=data[j][0][0][0][channel_idx], y=data[j][0][0][0][label_idx])
-            print("Writing to npy...{:3.1f}%".format(i / len(files) * 100))
-        print("Writing to npy...100%", end="\r")
+                out_str = os.path.join(sample_dir, 'epoch{}.npy'.format(j+1))
+                np.save(out_str, np.squeeze(data[j][0][0][0][channel_idx]))
+                labels.append(data[j][0][0][0][label_idx])
+            np.save(os.path.join(sample_dir, 'labels.npy'), labels)
+
+            print("Writing to npz...{:3.1f}%".format(i / len(files) * 100), end="\r")
+        print("Writing to npy...100%")
         print("Done.")
 
     def find_missing(self, file_set1, file_set2):

src/model/DataGenerator.py
@@ -45,15 +45,12 @@ class DataGenerator(keras.utils.Sequence):
         # Generate indexes of the batch
         indexes = self.indexes[index * self.batch_size:(index + 1) * self.batch_size]
 
-        # Find list of IDs
-        list_IDs_temp = [self.list_IDs[k] for k in indexes]
-
         # Generate data
-        X, y = self.__data_generation(list_IDs_temp)
+        X, y = self.__data_generation(indexes)
 
         return X, y
 
-    def __data_generation(self, list_IDs_temp):
+    def __data_generation(self, batch_inds):
         """
         Generates data containing batch_size samples
         :param list_IDs_temp:
@@ -64,15 +61,17 @@ class DataGenerator(keras.utils.Sequence):
         y = np.empty(self.batch_size, dtype=int)
 
         # Generate data
-        for i, ID in enumerate(list_IDs_temp):
+        for i, k in enumerate(batch_inds):
             # Store sample
-            data = np.load(ID)
-            X[i,] = data['x']
+            if FLAGS.downsample_rate > 1:
+                X[i,:] = np.reshape(np.load(self.list_IDs[k]), (-1, self.dim, FLAGS.downsample_rate, 1)).mean(axis=2)
+            else:
+                X[i,:] = np.reshape(np.load(self.list_IDs[k]), (-1, 1))
 
             # Store class
-            y[i] = data['y']
+            y[i] = self.labels[k]
 
-        return X, y
+        return X, keras.utils.to_categorical(y, num_classes=5)
 
     def on_epoch_end(self):
         """

src/model/DeepSleepNet.py
@@ -1,8 +1,8 @@
-from os import path
+import os
 from glob import glob
 
 import numpy as np
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 import tensorflow.keras as keras
 from sklearn.utils import class_weight
 from sklearn.metrics import confusion_matrix
@@ -10,7 +10,7 @@ from sklearn.metrics import confusion_matrix
 from src.model.DataGenerator import DataGenerator
 from data.DataPrepper import DataPrepper
 from src.model.RepresentationLearner import RepresentationLearner
-from src.model.SequenceResidualLearner import SequenceResidualLearner
+# from src.model.SequenceResidualLearner import SequenceResidualLearner
 from src.model.flags import FLAGS, EFFECTIVE_SAMPLE_RATE
 
 
@@ -26,33 +26,42 @@ class DeepSleepNet:
         self.n_classes = 5
 
         # data parsing
-        self.epoch_files = glob(path.join(FLAGS.data_dir, "*", "*.npz"))
+        self.epoch_files = glob(os.path.join(FLAGS.data_dir, "*", "epoch*.npy"))
+        self.label_files = glob(os.path.join(FLAGS.data_dir, "*", "labels.npy"))
         print("Generating labels")
-        self.labels = np.squeeze(np.array([np.load(f)['y'].astype("int") for f in self.epoch_files]))
-        train_split = int(len(self.epoch_files) * (1 - FLAGS.test_split))
-        val_split = train_split - int(train_split * FLAGS.val_split)
-        self.train_epochs = self.epoch_files[:val_split]
-        self.val_epochs = self.epoch_files[val_split:train_split]
-        self.test_epochs = self.epoch_files[train_split:]
-        self.train_gen = DataGenerator(self.train_epochs, self.labels)
-        self.val_gen = DataGenerator(self.train_epochs, self.labels)
-        self.test_gen = DataGenerator(self.train_epochs, self.labels)
+        self.labels = np.hstack([np.squeeze(np.load(f)) - 1 for f in self.label_files])
+
+        self.train_split = int(len(self.epoch_files) * (1 - FLAGS.test_split))
+        self.val_split = self.train_split - int(self.train_split * FLAGS.val_split)
+        self.train_gen = DataGenerator(self.epoch_files[:self.val_split], self.labels)
+        self.val_gen = DataGenerator(self.epoch_files[self.val_split:self.train_split], self.labels)
+        self.test_gen = DataGenerator(self.epoch_files[self.train_split:], self.labels)
 
     def train(self):
-        weights = class_weight.compute_class_weight('balanced', np.unique(self.labels), self.labels)
+        if not os.path.exists(FLAGS.checkpoint_dir):
+            os.makedirs(FLAGS.checkpoint_dir)
 
-        saver = keras.callbacks.ModelCheckpoint(filepath=path.join(FLAGS.checkpoint_dir, "rep_learn.h5"))
-        stopper = keras.callbacks.EarlyStopping(monitor="val_loss", restore_best_weights=True, patience=5)
+        weights = class_weight.compute_class_weight('balanced', np.unique(self.labels), self.labels)
+        class_weights = {i: 1 / weights[i] for i in range(self.n_classes)}
+
+        saver = keras.callbacks.ModelCheckpoint(filepath=os.path.join(FLAGS.checkpoint_dir, "rep_learn.h5"),
+                                                save_weights_only=True, save_best_only=True)
+        stopper = keras.callbacks.EarlyStopping(monitor='val_recall', restore_best_weights=True, patience=5, verbose=1)
 
         print("Pretraining Featurizer")
         model_rl = keras.Sequential([
-            RepresentationLearner(self.sampling_rate, use_dropout=True),
+            RepresentationLearner(self.sampling_rate),
             keras.layers.Dense(units=self.n_classes, activation="softmax")
         ])
-        model_rl.compile(optimizer="adam", loss="sparse_categorical_crossentropy")
-        model_rl.fit_generator(generator=self.train_gen, validation_data=self.val_gen, class_weight=weights,
-                               callbacks=[saver, stopper])
-        model_rl.evaluate_generator(generator=self.test_gen)
+        model_rl.compile(optimizer="rmsprop", loss="categorical_crossentropy",
+                         metrics=[keras.metrics.Precision(), keras.metrics.Recall()])
+        model_rl.fit(self.train_gen, validation_data=self.val_gen, class_weight=class_weights,
+                     callbacks=[stopper, saver], epochs=FLAGS.num_epochs_pretrain, verbose=1)
+        model_rl.evaluate(self.test_gen, verbose=0)
+
+        labs = np.argmax(model_rl.predict(self.test_gen), axis=1)
+        print(np.bincount(labs))
+        np.savez("labels.npz", pred=labs, gt=self.labels[self.train_split:])
 
 
 if __name__ == "__main__":